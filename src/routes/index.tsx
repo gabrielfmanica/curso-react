c863860125bc0c46d58516a19a0ccdc527998c05
gabriel.manica <gabriel.manica@ciss.com.br>
2022-08-02 14:24:17 -0300

    Ajustes de instalação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
 import  {Routes, Route, Navigate} from "react-router-dom";
+import {Button} from "@mui/material";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path = "/pagina-inicial" element = {<p>Página inical</p>}/>
+            <Route path = "/pagina-inicial" element = {<Button>Manica</Button>}/>
             <Route path = "/manica" element = {<p>Página MANICA</p>}/>
             <Route path = "*" element = { <Navigate to = "/pagina-inicial"/>}/>
         </Routes>
