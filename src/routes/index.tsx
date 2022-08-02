99f45c4629c4d0a7735a27c55afb4fb22f21beca
gabriel.manica <gabriel.manica@ciss.com.br>
2022-08-02 14:08:05 -0300

    Implementado o react-router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,8 @@ import  {Routes, Route, Navigate} from "react-router-dom";
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path = "/pagina-inicial" element = {<p>Página Inicial</p>}/>
+            <Route path = "/pagina-inicial" element = {<p>Página inical</p>}/>
+            <Route path = "/manica" element = {<p>Página MANICA</p>}/>
             <Route path = "*" element = { <Navigate to = "/pagina-inicial"/>}/>
         </Routes>
     )
