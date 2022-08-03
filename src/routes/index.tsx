fb3eff20e583fe1d8f0fdc86f9a5b8d87df1f7d9
gabriel.manica <gabriel.manica@ciss.com.br>
2022-08-03 13:31:48 -0300

    Testes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
 import  {Routes, Route, Navigate} from "react-router-dom";
-import {Button} from "@mui/material";
+import {Button, ButtonGroup} from "@mui/material";
 
 export const AppRoutes = () => {
     return (
         <Routes>
             <Route path = "/pagina-inicial" element = {<Button>Manica</Button>}/>
-            <Route path = "/manica" element = {<p>Página MANICA</p>}/>
+            <Route path = "/manica" element = {<p>Página MaNICA</p>}/>
             <Route path = "*" element = { <Navigate to = "/pagina-inicial"/>}/>
         </Routes>
     )
